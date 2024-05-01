c4181648983af576e12a6138df88d6e7da625b82
D <dmitry.mozhaev2012@gmail.com>
2024-05-01 18:33:10 +0300

    more code refactoring

--- go/integration/utils.go
+++ go/integration/utils.go
@@ -8,53 +8,31 @@ import (
     "double_up/controller"
 )
 
-func SendRequestPlayStart(db *sql.DB, method string, requestBody string) (*httptest.ResponseRecorder) {
-    req := httptest.NewRequest(method, "/play/start", bytes.NewBufferString(requestBody))
+func sendRequest(db *sql.DB, method string, url string, requestBody string, handlerFunc func(dbParam *sql.DB, wParam http.ResponseWriter, rParam *http.Request)) (*httptest.ResponseRecorder) {
+    req := httptest.NewRequest(method, url, bytes.NewBufferString(requestBody))
 
     // Create a response recorder to record the response
     rr := httptest.NewRecorder()
 
     // Convert controller.PlayStartHandler to a http.HandlerFunc
     handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        controller.PlayStartHandler(db, w, r)
+        handlerFunc(db, w, r)
     })
 
     // Serve the request using the handler and record the response
     handler.ServeHTTP(rr, req)
 
     return rr
+}
+
+func SendRequestPlayStart(db *sql.DB, method string, requestBody string) (*httptest.ResponseRecorder) {
+    return sendRequest(db, method, "/play/start", requestBody, controller.PlayStartHandler)
 }
 
 func SendRequestPlayContinue(db *sql.DB, method string, requestBody string) (*httptest.ResponseRecorder) {
-    req := httptest.NewRequest(method, "/play/continue", bytes.NewBufferString(requestBody))
-
-    // Create a response recorder to record the response
-    rr := httptest.NewRecorder()
-
-    // Convert controller.PlayContinueHandler to a http.HandlerFunc
-    handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        controller.PlayContinueHandler(db, w, r)
-    })
-
-    // Serve the request using the handler and record the response
-    handler.ServeHTTP(rr, req)
-
-    return rr
+    return sendRequest(db, method, "/play/continue", requestBody, controller.PlayContinueHandler)
 }
 
 func SendRequestWithdraw(db *sql.DB, method string, requestBody string) (*httptest.ResponseRecorder) {
-    req := httptest.NewRequest(method, "/withdraw/withdrawmoney", bytes.NewBufferString(requestBody))
-
-    // Create a response recorder to record the response
-    rr := httptest.NewRecorder()
-
-    // Convert controller.WithdrawHandler to a http.HandlerFunc
-    handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        controller.WithdrawHandler(db, w, r)
-    })
-
-    // Serve the request using the handler and record the response
-    handler.ServeHTTP(rr, req)
-
-    return rr
+    return sendRequest(db, method, "/withdraw/withdrawmoney", requestBody, controller.WithdrawHandler)
 }
